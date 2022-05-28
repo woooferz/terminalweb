46d43ea96cfe6b7bb1b7e6af35e986ad54a19e88
woooferz <85282355+woooferz@users.noreply.github.com>
2022-05-28 12:35:03 +1000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Terminal Themed Website
+
+mm, yes!
---
+++ js/main.js
@@ -0,0 +1,380 @@
+// Customize right-click menu
+
+/* VARIABLES */
+
+const version = "v0.3";
+
+const prefixText = `[t1]you[/t1][t2]@[/t2][t3]woofterm-${version} $ [/t3]`;
+
+/* END OF VARIABLES */
+
+const output = document.getElementById("output");
+const input = document.getElementById("terminalInput");
+const prefix = document.getElementById("prefix");
+
+let availableCommands = [
+  "waffle",
+  "help",
+  "colors",
+  "colours",
+  "clear",
+  "wfetch",
+  "startup",
+  "credits",
+  "changelog",
+  "history",
+  "open",
+  "",
+]; // Sets the available commands for the red stuff to work.
+
+let advancedAvailableCommands = ["open "];
+
+let commands = [];
+let backIndex = 0;
+
+function addRawLine(text) {
+  output.innerHTML = output.innerHTML + text;
+}
+function colorize(text) {
+  let colorizedText = text;
+  colorizedText = colorizedText
+    .replaceAll("[cr]", '<div class="colour red-stuff">')
+    .replaceAll("[/cr]", "</div>"); // RED
+  colorizedText = colorizedText
+    .replaceAll("[cb]", '<div class="colour blue-stuff">')
+    .replaceAll("[/cb]", "</div>"); // BLUE
+  colorizedText = colorizedText
+    .replaceAll("[cy]", '<div class="colour yellow-stuff">')
+    .replaceAll("[/cy]", "</div>"); // YELLOW
+  colorizedText = colorizedText
+    .replaceAll("[cg]", '<div class="colour green-stuff">')
+    .replaceAll("[/cg]", "</div>"); // GREEN
+  colorizedText = colorizedText
+    .replaceAll("[clb]", '<div class="colour lightblue-stuff">')
+    .replaceAll("[/clb]", "</div>"); // LIGHT BLUE
+  colorizedText = colorizedText
+    .replaceAll("[clg]", '<div class="colour lightgreen-stuff">')
+    .replaceAll("[/clg]", "</div>"); // LIGHT BLUE
+  colorizedText = colorizedText
+    .replaceAll("[t1]", '<div class="colour term1-stuff">')
+    .replaceAll("[/t1]", "</div>"); // LIGHT BLUE
+  colorizedText = colorizedText
+    .replaceAll("[t2]", '<div class="colour term2-stuff">')
+    .replaceAll("[/t2]", "</div>"); // LIGHT BLUE
+  colorizedText = colorizedText
+    .replaceAll("[t3]", '<div class="colour term3-stuff">')
+    .replaceAll("[/t3]", "</div>"); // LIGHT BLUE
+
+  if (colorizedText.match(/\[click-\w+\]/g)) {
+    // console.log("clicky");
+    // console.log(
+    //   colorizedText
+    //     .match(/\[click-\w+\]/g)[0]
+    //     .replace("[click-", "")
+    //     .replace("]", "")
+    // );
+    const cmdName = colorizedText
+      .match(/\[click-\w+\]/g)[0]
+      .replace("[click-", "")
+      .replace("]", "");
+    // console.log(cmdName);
+    colorizedText = colorizedText.replaceAll(
+      `[click-${cmdName}]`,
+      `<div class='inline click' woofclick='${cmdName}'>`
+    );
+  }
+  if (
+    colorizedText.match(
+      /\[link-https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)\]/g
+    )
+  ) {
+    // console.log("clicky");
+    // console.log(
+    //   colorizedText
+    //     .match(/\[click-\w+\]/g)[0]
+    //     .replace("[click-", "")
+    //     .replace("]", "")
+    // );
+    const linkUrl = colorizedText
+      .match(
+        /\[link-https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)\]/g
+      )[0]
+      .replace("[link-", "")
+      .replace("]", "");
+    // console.log(cmdName);
+    colorizedText = colorizedText.replaceAll(
+      `[link-${linkUrl}]`,
+      `<a href="${linkUrl}" target=\"_blank">`
+    );
+  }
+  colorizedText = colorizedText.replace("[/link]", "</a>");
+  colorizedText = colorizedText.replace("[/click]", "</div>");
+  if (colorizedText.match(/\[\d+s\/\]/g)) {
+    colorizedText.match(/\[\d+s\/\]/g).forEach((element) => {
+      let numOfSpaces = element.replace(/(\[|s|\/|\])/g, "");
+
+      if (numOfSpaces.match(/\d+/g)) {
+        let spaces = "";
+        for (var i = 0; i < parseInt(numOfSpaces); i++) {
+          spaces = spaces + "[s/]";
+          //   console.log(spaces);
+        }
+        colorizedText = colorizedText.replace(
+          "[" + numOfSpaces + "s/]",
+          spaces
+        );
+      }
+    });
+  } // I don't know how to read this, i wrote it btw
+
+  colorizedText = colorizedText.replaceAll("[n/]", "<br/>");
+  colorizedText = colorizedText.replaceAll("[s/]", "&nbsp;");
+  colorizedText = colorizedText.replaceAll("[&09&]", "");
+
+  return colorizedText;
+}
+function addLine(text) {
+  addRawLine('<div class="line">' + colorize(text) + "</div>");
+}
+function clearOutput() {
+  output.innerHTML = "";
+}
+function setInput(text) {
+  input.value = text;
+  var that = input;
+  setTimeout(function () {
+    that.selectionStart = that.selectionEnd = 10000;
+  }, 0);
+  const event = new Event("input");
+  input.dispatchEvent(event);
+}
+function refocus() {
+  input.focus();
+}
+function checkIfStringStartsWith(str, substrs) {
+  return substrs.some((substr) => str.startsWith(substr));
+}
+
+function commandExists(command) {
+  return (
+    availableCommands.indexOf(command) >= 0 ||
+    checkIfStringStartsWith(command, advancedAvailableCommands)
+  );
+}
+commandExists("hello");
+function addCommandLine() {
+  const doesCommandExists = commandExists(input.value);
+  addLine(
+    prefixText +
+      (doesCommandExists
+        ? input.value
+        : "<div class='error inline'>" + input.value + "</div>")
+  );
+}
+function openTab(url) {
+  window.open(url, "_blank").focus();
+}
+function helpCommand(cmd, desc) {
+  addLine(`[clb][click-${cmd}]${cmd}[/click][/clb][n/][3s/]${desc}`);
+}
+input.addEventListener("input", (event) => {
+  event.target.setAttribute(
+    "size",
+    event.target.value.length < 1 ? 1 : event.target.value.length
+  );
+  event.target.setAttribute(
+    "maxlength",
+    event.target.value.length < 1 ? 3 : event.target.value.length + 2
+  );
+  //   console.log(event.target.value);
+  if (commandExists(event.target.value)) {
+    try {
+      event.target.classList.remove("error");
+    } catch {}
+  } else {
+    event.target.classList.add("error");
+  }
+});
+
+refocus();
+
+clearOutput();
+
+prefix.innerHTML = colorize(prefixText);
+function advancedCommands(cmd) {
+  let command = cmd.split(" ")[0];
+  let args = cmd.split(" ");
+  args.shift();
+
+  //   addLine("CMD: " + command);
+  //   addLine("Args: " + args);
+
+  switch (command) {
+    case "open":
+      addLine(
+        `[cg]Opening ${
+          args.join("%20") == "" ? "nothing??" : args.join("%20")
+        }...[/cg]`
+      );
+      openTab(args.join("%20"));
+      break;
+    default:
+      return false;
+  }
+  return true;
+}
+function commandHandler(command, cmdline = true) {
+  if (cmdline) addCommandLine();
+  backIndex = 0;
+
+  switch (command) {
+    case "waffle":
+      addLine("Yessir!");
+      break;
+    case "changelog":
+      addLine("Changelog:");
+      addLine("[2s/]v0.3:");
+      addLine("[4s/]Updated colours");
+      addLine("[4s/]Added support for advanced commands");
+
+      addLine("[2s/]v0.2.5:");
+      addLine("[4s/]Made it simpler to use links");
+      addLine("[2s/]v0.2.4:");
+      addLine("[4s/]Added history command");
+      addLine("[2s/]v0.2.3:");
+      addLine("[4s/]Added changelog command");
+      addLine("[2s/]v0.2.2:");
+      addLine("[4s/]Added credits command");
+      break;
+    case "help":
+      helpCommand("help", "Outputs help message (AKA [cb]this[/cb])");
+      helpCommand("waffle", "Says if computer wants waffles are not");
+      helpCommand("colours", "Displays available colours");
+      helpCommand("clear", "Clear the screen");
+      helpCommand("wfetch", "Waffle Fetch displays info");
+      helpCommand("startup", "Says startup message");
+      helpCommand("credits", "Displays credits");
+      helpCommand("changelog", "Changelog for Woof Terminal");
+      helpCommand("history", "Shows command history of current session");
+      helpCommand("open", "Opens a new tab with passed in url");
+
+      break;
+    case "history":
+      addLine("[cy]History:[/cy]");
+      addLine(commands.join("[n/]"));
+      break;
+    case "startup":
+      addLine("Welcome to Woof Terminal " + version + "!");
+      addLine(
+        "[cy]TIPS:[/cy] Use the [clb][click-help]help[/click][/clb] command to recieve help!"
+      );
+      break;
+    case "credits":
+      addLine("Thanks to: ");
+      addLine("[3s/][cy]Waffles[/cy] for keeping me alive");
+      addLine(
+        "[3s/]JetBrains for JetBrains Mono [clb][link-https://www.jetbrains.com/lp/mono/] Link[/link][/clb]"
+      );
+      addLine(
+        "[3s/]Ascii.co.uk for Waffle Ascii Art [clb][link-https://ascii.co.uk/art/waffle] Link[/link][/clb]"
+      );
+      break;
+    case "waffles":
+      addLine("Hello!");
+      break;
+    case "colors":
+    case "colours":
+      addLine("[cb]BLUE[/cb]");
+      addLine("[clb]LIGHT BLUE[/clb]");
+      addLine("[cg]GREEN[/cg]");
+      addLine("[cr]RED[/cr]");
+      addLine("[cy]YELLOW[/cy]");
+      break;
+    case "wfetch":
+      const wfetch = `
+    [cy][4s/]_.-------._[/cy][5s/]
+    [n/]        
+    [cy][2s/].' _|_|_|_|_ '.[/cy][3s/]
+    [n/]    
+    [cy][s/]/ _|_|_|_|_|_|_ \\[/cy][2s/] [clb]Terminal:[/clb] [cg]Woof Terminal ${version}[/cg] 
+    [n/]
+    [cy]| |_|_|_|_|_|_|_| |[/cy][s/]     [clb]Theme:[/clb] [cg]Waffle Basic Theme[/cg]
+    [n/]
+    [cy]|_|_|_|_|_|_|_|_|_|[/cy][s/]     [clb]My Website:[/clb] [cg][link-https://wooferz.dev]wooferz.dev[/link][/cg]
+    [n/]      
+    [cy]| |_|_|_|_|_|_|_| |[/cy][s/]     [clb]Hardware:[/clb] [cg]Browser[/cg]
+    [n/]      
+    [cy]| |_|_|_|_|_|_|_| |[/cy][s/]
+    [n/]     
+    [cy][s/]\\ -|_|_|_|_|_|- /[/cy][2s/]
+    [n/] 
+    [cy][2s/]'. -|_|_|_|- .'[/cy][3s/]
+    [n/]    
+    [cy][4s/]\`---------\`[/cy][5s/]
+    [n/]      
+      `;
+      addLine(wfetch);
+      break;
+    case "clear":
+      clearOutput();
+      break;
+    case "":
+      break;
+    default:
+      if (!advancedCommands(command))
+        addLine(
+          "[cr]Unknown Command! Use [click-help]help[/click] for help![/cr]"
+        );
+      break;
+  }
+  window.scrollTo(0, document.body.scrollHeight);
+}
+
+input.onkeydown = function (e) {
+  if (e.key == "Enter") {
+    commandHandler(input.value);
+    if (input.value != "") {
+      commands.push(input.value);
+    }
+    setInput("");
+    refocus();
+  } else if (e.key == "ArrowUp") {
+    backIndex = backIndex >= commands.length ? backIndex : backIndex + 1;
+    setInput(backIndex <= 0 ? "" : commands[commands.length - backIndex]);
+    refocus();
+  } else if (e.key == "ArrowDown") {
+    backIndex = backIndex <= 0 ? 0 : backIndex - 1;
+
+    setInput(backIndex <= 0 ? "" : commands[commands.length - backIndex]);
+    refocus();
+  }
+  //   addLine(backIndex);
+};
+
+commandHandler("startup", false);
+
+// document.getElementsByTagName("body")[0].addEventListener("change", (event) => {
+//   const woofclicks = document.querySelectorAll(".click");
+//   console.log(woofclicks);
+//   woofclicks.forEach((woofclick) => {
+//     woofclick.onclick = function (event) {
+//       setInput(this.getAttribute("woofclick"));
+//     };
+//   });
+// });
+const woofclicks = document.querySelectorAll(".click");
+woofclicks.forEach((woofclick) => {
+  woofclick.onclick = function (event) {
+    setInput(this.getAttribute("woofclick"));
+  };
+});
+document.addEventListener("DOMSubtreeModified", (e) => {
+  const woofclicks = document.querySelectorAll(".click");
+  woofclicks.forEach((woofclick) => {
+    woofclick.onclick = function (event) {
+      setInput(this.getAttribute("woofclick"));
+    };
+  });
+});
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
